f20684e5c9f9464507fb971d568da9e866a957e1
Alexandr Katrutsa <amkatrutsa@yandex.ru>
2014-06-14 17:52:55 +0400

    Add normalisation all sets and target vectors

--- code/DivideLearnTest.m
+++ code/DivideLearnTest.m
@@ -1,6 +1,6 @@
 function [ learn_X, test_X, learn_y, test_y ] = DivideLearnTest( X, y )
 % Function divides data set X and target vector y on the learning and 
-% test data sets 
+% test data sets and normalise both sets and target vectors
 %
 % Input:
 % X - [m, n] - given data set
@@ -17,5 +17,13 @@ learn_X = X(1:learn_size, :);
 test_X = X((learn_size + 1):end, :);
 learn_y = y(1:learn_size, :);
 test_y = y((learn_size + 1):end, :);
+
+len = sum(learn_X.^2).^0.5;
+learn_X = learn_X ./ repmat(len, size(learn_X, 1), 1);
+learn_y = learn_y ./ norm(learn_y);
+
+len = sum(test_X.^2).^0.5;
+test_X = test_X ./ repmat(len, size(test_X, 1), 1);
+test_y = test_y ./ norm(test_y);
 end
 
