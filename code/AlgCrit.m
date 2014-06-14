6e14c87df24455ac98bfe04d7c09b0f28ac78955
Alexandr Katrutsa <amkatrutsa@yandex.ru>
2014-06-14 17:50:24 +0400

    Add return learn and test errors, numbers of parametersin function AlgCrit

--- code/AlgCrit.m
+++ code/AlgCrit.m
@@ -1,4 +1,4 @@
-function [ matAlgCrit ] = AlgCrit(alg, crit, objects, features, parameters)
+function [ matAlgCrit, learn_error, test_error, num_parameters ] = AlgCrit(alg, crit, objects, features, parameters)
 % Function computes values of every considered criteria for result
 % given every feature selection method
 %
@@ -24,29 +24,40 @@ function [ matAlgCrit ] = AlgCrit(alg, crit, objects, features, parameters)
                 
 iter = parameters.iter;
 matAlgCrit = zeros(length(alg), length(crit));
+learn_error = zeros(1, length(alg));
+test_error = zeros(1, length(alg));
+num_parameters = zeros(1, length(alg));
 for it = 1:iter
     parameters.target = randi(1.5 * objects, objects, 1);
     X = CreateData(objects, features, parameters);
     y = parameters.target;
-    len = sum(X.^2).^0.5;
-    X = X ./ repmat(len, size(X, 1), 1);
-    y = y ./ norm(y);
-    beta = lscov(X, y);
-    rss_all = sumsqr(y - X * beta);
-    tss_all = sumsqr(y - mean(y));
+    [ learn_X, test_X, learn_y, test_y ] = DivideLearnTest(X, y);    
+    beta = lscov(learn_X, learn_y);
+    rss_all = sumsqr(learn_y - learn_X * beta);
+    tss_all = sumsqr(learn_y - mean(learn_y));
     par.s_0 = parameters.s_0;
     par.rss = rss_all;
     par.tss = tss_all;
     for i = 1:length(alg)
-        X_sh = X;
-        w = feval(alg{i}, X, y);
-        idx_del = abs(w) < 10^(-6);
+        learn_X_shrink = learn_X;
+        test_X_shrink = test_X;
+        w = feval(alg{i}, learn_X, learn_y);
+        idx_del = abs(w) < 10^(-5); %10^(-6)
         w(idx_del) = [];
-        X_sh(:, idx_del) = [];
+        learn_X_shrink(:, idx_del) = [];
+        test_X_shrink(:, idx_del) = [];
+        learn_error(i) = learn_error(i) + sumsqr(learn_y - learn_X_shrink * w);
+        test_error(i) = test_error(i) + sumsqr(test_y - test_X_shrink * w);
+        num_parameters(i) = num_parameters(i) + size(w, 1);
         for j = 1:length(crit)                
-            matAlgCrit(i, j) = matAlgCrit(i, j) + feval(crit{j}, X_sh, y, w, par);
+            matAlgCrit(i, j) = matAlgCrit(i, j) + feval(crit{j}, learn_X_shrink, ...
+                                                    learn_y, w, par);
         end
+        
     end
 end
 matAlgCrit = matAlgCrit / iter;
+learn_error = learn_error / iter;
+test_error = test_error / iter;
+num_parameters = num_parameters / iter;
 end
