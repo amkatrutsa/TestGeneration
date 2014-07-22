67cfbcb98bd205705d00eeee068c09ebc7dc1042
Alexandr Katrutsa <amkatrutsa@yandex.ru>
2014-07-22 16:21:29 +0400

    Add function to find the best FSM according to given criterion

---
+++ code/IdxBestFSM.m
@@ -0,0 +1,27 @@
+function [ idxFSM ] = IdxBestFSM( X, y, crit, alg, param )
+% Function finds the index of the best feature selection method (FSM) 
+% among the FSM from alg according to the criterion crit  
+% 
+% Input:
+% X - [m, n] - data set
+% y - [m, 1] - target vector
+% crit - string - selected external criterion
+% alg - {cell array} - the list of the considered FSM
+% param - structure with additional parameters:
+%           s_0 - [1, 1] - limit error
+%           
+% Output:
+% idxFSM - [1, 1] - index of the best FSM from the list alg according to
+% the criterion crit
+
+crit_res = zeros(1, length(alg));
+for i = 1:length(alg)
+    X_shrink = X;
+    w = feval(alg{i}, X, y);
+    idx_del = abs(w) < 10^(-5); %10^(-6)
+    w(idx_del) = [];
+    X_shrink(:, idx_del) = [];
+    crit_res(i) = feval(crit, X_shrink, y, w, param);
+end
+[~, idxFSM] = min(crit_res);
+end
